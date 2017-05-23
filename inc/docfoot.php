b463a0254763e8934387709b733017a6e92e9733
mk-kimjh <mk.kimjh@gmail.com>
2017-05-23 15:22:58 +0900

    레이아웃 작성 중... 20%

---
+++ inc/docfoot.php
@@ -0,0 +1,3 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/js/common.js"></script>
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+
+	<nav id="top-nav" class="visible-lg">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6">
+					계열사 사이트
+				</div>
+				<div class="col-xs-6">
+					빠른 메뉴
+				</div>
+			</div>
+		</div>
+	</nav>
+	
+	<header id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-3">
+					<button type="button" class="btn btn-link"><i class="icon-menu"></i></button>
+					<h1><a href="/"><img src="/assets/images/logo.png" alt="우정비에스씨"></a></h1>
+					<a class="btn btn-link" href="#"><i class="icon-wrench"></i></a>
+				</div>
+				<div class="hidden-xs col-md-9">
+					<nav id="nav">
+						<ul>
+							<li>
+								<a href="#">이노서비스</a>
+								<ul>
+									<li><a href="#">사업비전</a></li>
+									<li><a href="#">홍보/영상</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">기술서비스상품</a>
+								<ul>
+									<li><a href="#">유지보수 서비스</a></li>
+									<li><a href="#">과산화수소멸균 서비스</a></li>
+									<li><a href="#">장비검교정 서비스</a></li>
+									<li><a href="#">우정특성화 서비스</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">견적문의</a>
+								<ul>
+									<li><a href="#">기술서비스 견적문의</a></li>
+									<li><a href="#">RBDS가능 알아보기</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">우정콜센터</a>
+								<ul>
+									<li><a href="#">공지사항</a></li>
+									<li><a href="#">A/S현황 조회</a></li>
+									<li><a href="#">검교정주기 조회</a></li>
+									<li><a href="#">1:1 문의하기</a></li>
+									<li><a href="#">고객 만족도 평가</a></li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+			<div class="row visible-lg">
+				<div class="col-xs-3">1522-1277</div>
+			</div>
+		</div>
+	</header>
+	
+	<aside id="aside-nav">
+		<div class="nav-content">
+			<div class="nav-header">
+				
+			</div>
+			<div class="nav-body">
+				
+			</div>
+			<div class="nav-footer">
+				
+			</div>
+		</div>
+	</aside>
+	
+	<main id="content">
+		#content		
+	</main>
+	
+	<footer id="footer">
+		
+	</footer>
+	
+</body>
+</html>
