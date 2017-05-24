82a8a3fb1ebe631c225a5b5b5a7ceed84b2d8b40
mk-kimjh <mk.kimjh@gmail.com>
2017-05-24 16:07:12 +0900

    레이아웃 작성 중... 60%

--- inc/dochead.php
+++ inc/dochead.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>우정BSC</title>
+<title>이노서비스</title>
 <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!-- link href="/assets/fontello/css/fontello.css" rel="stylesheet" -->
 <link href="/assets/css/default.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
 <link href="/assets/css/main.css" rel="stylesheet">
 </head>
-<body>
+<body class="nav-expanded">
 
 	<nav id="top-nav" class="visible-lg">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-6">
-					계열사 사이트
+					<div class="affiliates">
+						<ul>
+							<li><a href="#">우정BSC</a></li>
+							<li><a href="#">우정E&amp;C</a></li>
+							<li><a href="#">INNOSERVICE</a></li>
+							<li><a href="#">CROWISE</a></li>
+							<li><a href="#">BSMS</a></li>
+						</ul>
+					</div>
 				</div>
 				<div class="col-xs-6">
-					빠른 메뉴
+					<div class="quick">
+						<ul>
+							<li><a href="#">A/S현황 조회</a></li>
+							<li><a href="#">견적문의</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -19,78 +32,152 @@
 	<header id="header">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-md-3">
-					<button type="button" class="btn btn-link"><i class="icon-menu"></i></button>
-					<h1><a href="/"><img src="/assets/images/logo.png" alt="우정비에스씨"></a></h1>
-					<a class="btn btn-link" href="#"><i class="icon-wrench"></i></a>
+				<div class="col-xs-12 col-lg-3">
+					<button type="button" id="btn-nav-open" class="btn btn-link hidden-lg"><i class="icon-menu"></i><span class="sr-only">사이드메뉴 열기</span></button>
+					<h1 class="logo"><a href="/"><img src="/assets/images/logo.png" alt="우정비에스씨"></a></h1>
+					<a id="btn-search-as" class="btn btn-link hidden-lg" href="#"><i class="icon-wrench"></i><span class="sr-only">A/S조회</span></a>
 				</div>
-				<div class="hidden-xs col-md-9">
+				<div class="visible-lg col-lg-9">
 					<nav id="nav">
 						<ul>
-							<li>
+							<li class="active">
 								<a href="#">이노서비스</a>
 								<ul>
-									<li><a href="#">사업비전</a></li>
-									<li><a href="#">홍보/영상</a></li>
+									<li><a href="#">사업비전<span></span></a></li>
+									<li><a href="#">홍보/영상<span></span></a></li>
 								</ul>
 							</li>
 							<li>
 								<a href="#">기술서비스상품</a>
 								<ul>
-									<li><a href="#">유지보수 서비스</a></li>
-									<li><a href="#">과산화수소멸균 서비스</a></li>
-									<li><a href="#">장비검교정 서비스</a></li>
-									<li><a href="#">우정특성화 서비스</a></li>
+									<li><a href="#">유지보수 서비스<span></span></a></li>
+									<li><a href="#">과산화수소멸균 서비스<span></span></a></li>
+									<li><a href="#">장비검교정 서비스<span></span></a></li>
+									<li><a href="#">우정특성화 서비스<span></span></a></li>
 								</ul>
 							</li>
 							<li>
 								<a href="#">견적문의</a>
 								<ul>
-									<li><a href="#">기술서비스 견적문의</a></li>
-									<li><a href="#">RBDS가능 알아보기</a></li>
+									<li><a href="#">기술서비스 견적문의<span></span></a></li>
+									<li><a href="#">RBDS가능 알아보기<span></span></a></li>
 								</ul>
 							</li>
 							<li>
 								<a href="#">우정콜센터</a>
 								<ul>
-									<li><a href="#">공지사항</a></li>
-									<li><a href="#">A/S현황 조회</a></li>
-									<li><a href="#">검교정주기 조회</a></li>
-									<li><a href="#">1:1 문의하기</a></li>
-									<li><a href="#">고객 만족도 평가</a></li>
+									<li><a href="#">공지사항<span></span></a></li>
+									<li><a href="#">A/S현황 조회<span></span></a></li>
+									<li><a href="#">검교정주기 조회<span></span></a></li>
+									<li><a href="#">1:1 문의하기<span></span></a></li>
+									<li><a href="#">고객 만족도 평가<span></span></a></li>
 								</ul>
 							</li>
 						</ul>
 					</nav>
 				</div>
 			</div>
-			<div class="row visible-lg">
-				<div class="col-xs-3">1522-1277</div>
+			<div class="as">
+				<h5>A/S 고객 서비스센터</h5>
+				<p class="num"><strong>1522-1277</strong></p>
+				<dl class="mail">
+					<dt>E-mail</dt>
+					<dd><a href="mailto:info@woojungbsc.co.kr" target="_blank">info@woojungbsc.co.kr</a></dd>
+				</dl>
+				<p class="info">
+					평일(월~금) AM09:00 ~ PM06:00<br>
+					주말 및 공휴일은 휴무입니다.
+				</p>
 			</div>
 		</div>
 	</header>
 	
-	<aside id="aside-nav">
+	<aside id="nav-aside">
 		<div class="nav-content">
 			<div class="nav-header">
-				
+				<button type="button" id="btn-nav-close" class="btn btn-link"><img src="/assets/images/ico_close.png" alt="닫기"></button>
+				<div class="quick">
+					<ul>
+						<li><a href="#">A/S 현황조회</a></li>
+						<li><a href="#">견적문의</a></li>
+					</ul>
+				</div>
 			</div>
 			<div class="nav-body">
-				
+				<nav class="nav">
+					<ul>
+						<li class="active">
+							<a href="#">이노서비스</a>
+							<ul>
+								<li><a href="#">사업비전</a></li>
+								<li><a href="#">홍보/영상</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">기술서비스상품</a>
+							<ul>
+								<li><a href="#">유지보수 서비스</a></li>
+								<li><a href="#">과산화수소멸균 서비스</a></li>
+								<li><a href="#">장비검교정 서비스</a></li>
+								<li><a href="#">우정특성화 서비스</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">견적문의</a>
+							<ul>
+								<li><a href="#">기술서비스 견적문의</a></li>
+								<li><a href="#">RBDS가능 알아보기</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">우정콜센터</a>
+							<ul>
+								<li><a href="#">공지사항</a></li>
+								<li><a href="#">A/S현황 조회</a></li>
+								<li><a href="#">검교정주기 조회</a></li>
+								<li><a href="#">1:1 문의하기</a></li>
+								<li><a href="#">고객 만족도 평가</a></li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
 			</div>
 			<div class="nav-footer">
-				
+				<div class="as">
+					<h5>A/S 고객 서비스센터</h5>
+					<p class="num"><strong>1522-1277</strong></p>
+					<dl class="mail">
+						<dt>E-mail</dt>
+						<dd><a href="mailto:info@woojungbsc.co.kr" target="_blank">info@woojungbsc.co.kr</a></dd>
+					</dl>
+					<p class="info">
+						평일(월~금) AM09:00 ~ PM06:00<br>
+						주말 및 공휴일은 휴무입니다.
+					</p>
+				</div>
 			</div>
 		</div>
 	</aside>
 	
-	<main id="content">
+	<main id="content" style="height: 2000px;">
 		#content		
 	</main>
 	
 	<footer id="footer">
 		
 	</footer>
+	
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+	<script>
+		(function($) {
+			$(document).on('mouseenter focusin', '#header .container #nav > ul > li > a', function() {
+				$('body').addClass('nav-expanded');
+			});
+			$(document).on('mouseleave', '#header', function() {
+				$('body').removeClass('nav-expanded');
+			});
+		})(jQuery);
+	</script>
 	
 </body>
 </html>
