52899da20c594b32f202d80b940cd6f0b28c9e59
mk-kimjh <mk.kimjh@gmail.com>
2017-05-25 09:53:34 +0900

    레이아웃 작성 완료

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
 <link href="/assets/css/main.css" rel="stylesheet">
 </head>
-<body class="nav-expanded">
+<body>
 
 	<nav id="top-nav" class="visible-lg">
 		<div class="container">
@@ -92,7 +92,7 @@
 		</div>
 	</header>
 	
-	<aside id="nav-aside">
+	<aside id="nav-aside" class="hidden-lg">
 		<div class="nav-content">
 			<div class="nav-header">
 				<button type="button" id="btn-nav-close" class="btn btn-link"><img src="/assets/images/ico_close.png" alt="닫기"></button>
@@ -159,12 +159,56 @@
 		</div>
 	</aside>
 	
-	<main id="content" style="height: 2000px;">
+	<main id="content" style="height: 1000px;">
 		#content		
 	</main>
 	
 	<footer id="footer">
-		
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-9 col-lg-10">
+					<div class="address">
+						<dl>
+							<dt>주소</dt>
+							<dd><address>경기도 수원시 영통구 광교로 145 차세대융합기술연구원 B동 3층 (주)우정비에스씨</address></dd>
+						</dl>
+					</div>
+					<div class="contact">
+						<dl>
+							<dt>TEL</dt>
+							<dd>031-888-9369</dd>
+							<dt>FAX</dt>
+							<dd>031-888-9368</dd>
+							<dt>E-mail</dt>
+							<dd><a href="mailto:wj@woojungbsc.co.kr" target="_blank">wj@woojungbsc.co.kr</a></dd>
+						</dl>
+					</div>
+					<div class="copyright">
+						<p>
+							COPYRIGHT (C) 2017 WOOJUNGBSC.,CO.LTD.<br class="visible-xs">
+							ALL RIGHTS RESERVED.<br class="hidden-lg">
+							SITE BY <a href="http://movementk.com" target="_blank">MOVEMENT K GROUP</a>.
+						</p>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-lg-2">
+					<div class="family">
+						<div class="dropup">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
+								FAMILY SITE
+								<span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li><a href="#">Separated link</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 	
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
