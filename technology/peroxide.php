9a43f41ad4cf57b95ef91e0a332cbd379568a61d
kimyh5392 <mk.kimyh@gmail.com>
2017-05-31 18:27:05 +0900

    기술서비스상품 > 유지보수 서비스 , 과산화수소멸균 서비스(과산화수소증기멸균) 완료 공간멸균서비스 작업중. . .

---
+++ technology/peroxide.php
@@ -0,0 +1,303 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/technology.css" rel="stylesheet">
+</head>
+<body class="sub">
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/top-nav.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/nav-aside.php'); ?>
+	
+	<!-- Sub Content Start -->
+	<main id="content" class="technology">
+        <!-- Page Header -->
+	    <div class="page-header">
+            <div class="page-info">
+                <h2 class="hidden-xs">SERVICE</h2>
+                <p>기술서비스 상품</p>
+                <div class="btn-link-area">
+                    <a href="#" class="btn-link pre-btn" role="button">
+                        <img src="/assets/images/sub/left_link_btn.png" alt="이전 페이지">
+                        <span class="sr-only">이전 페이지</span>
+                    </a>
+                    <a href="#" class="btn-link next-btn" role="button">
+                        <img src="/assets/images/sub/right_link_btn.png" alt="다음 페이지">
+                        <span class="sr-only">이전 페이지</span>
+                    </a>
+                </div>
+	        </div>
+            <div class="lnb dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    과산화수소멸균 서비스
+                    <i class="more-icon">
+                        <span class="sr-only">더보기</span>
+                    </i>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a href="/technology/maintenance.php">유지보수 서비스</a></li>
+                    <li class="active"><a href="/technology/peroxide.php">과산화수소멸균 서비스</a></li>
+                    <li><a href="#">장비검교정 서비스</a></li>
+                    <li><a href="#">우정특성화 서비스</a></li>
+                </ul>
+            </div>
+	    </div>
+	    <!-- //Page Header -->
+	    <section class="peroxide">
+            <div class="section-header">
+                <h3 class="page-title">과산화수소멸균 서비스</h3>
+            </div>
+            <div class="section-content">
+                <div class="container">
+                    <nav class="nav-tabs tab-link-list">
+                        <ul>
+                            <li class="active">
+                                <a href="/technology/peroxide.php">과산화수소증기멸균</a>
+                            </li>
+                            <li>
+                                <a href="/technology/space.php">공간멸균서비스</a>
+                            </li>
+                            <li>
+                                <a href="/technology/process.php">멸균공정개발서비스</a>
+                            </li>
+                            <li>
+                                <a href="/technology/automation.php">멸균자동화 설비 공사</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <article class="peroxide-info">
+                        <div class="article-header">
+                            <h4 class="content-title">과산화수소 증기 멸균이란?</h4>
+                            <hr>
+                            <p class="summary">
+                                과산화수소(30~35%)는 US, EPA에 등록된 소독제로,<br class="visible-xs">
+                                내생포자, 마이코박테리아, 식물성박테리아, 비정형세균,<br class="hidden-lg">
+                                바이러스, 곰팡이와 같은<br class="visible-lg"> 미생물군에 효과적이고<br class="visible-xs">
+                                일반 소독제로 제거하기 어려운 Methicillin Resistant<br class="visible-xs">
+                                Staphylococcus<br class="visible-md"> aureus(MRSA), Vancomycin<br class="visible-xs">
+                                Resistant Enterococcus(VRE), Clostridium difficile,<br class="visible-xs">
+                                Acinetobacter,<br class="visible-md">  Norovirus와 같은 병원성<br class="visible-xs">
+                                감염체의 박멸이 가능합니다.<br>
+                                이에 현재 의약품 생산, 바이오 신약 개발, 식품 생산,<br class="visible-xs">
+                                병원, 실험동물실 등에 바이오 멸균제로서 광범위하게<br class="visible-xs">
+                                사용되고 있습니다.
+
+                            </p>
+                        </div>
+                        <div class="article-content">
+                            <div class="decimal-list">
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-lg-4">
+                                        <h5 class="list-title re">H2O2 증기 멸균의 장점</h5>
+                                        <ol class="decimal-li first-ol">
+                                            <li>
+                                                <i class="circle-num re">01</i>
+                                                광범위한 멸균력
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">02</i>
+                                                뛰어난 재질 적합성
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">03</i>
+                                                공조기 별 멸균 가능
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">04</i>
+                                                잔류물 없음
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">05</i>
+                                                검증가능
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">06</i>
+                                                빠른 멸균 속도
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">07</i>
+                                                환경 친화적
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">08</i>
+                                                제어 가능한 멸균공정
+                                            </li>
+                                            <li>
+                                                <i class="circle-num re">09</i>
+                                                작업자 안전유지
+                                            </li>
+                                        </ol>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-lg-4">
+                                        <h5 class="list-title or">H2O2 증기 멸균의  신뢰성</h5>
+                                        <ol class="decimal-li">
+                                            <li>
+                                                <i class="circle-num or">01</i>
+                                                Safety, Assurance, Material 
+                                                Compatibility, Reproducibility 보장
+                                            </li>
+                                            <li>
+                                                <i class="circle-num or">02</i>
+                                                USP에 명시된 균주(Geobacillus 
+                                                Stearothermopilus)를 이용하여 검증 
+                                                가능(GMP/GLP)
+                                            </li>
+                                            <li>
+                                                <i class="circle-num or">03</i>
+                                                6-log reduction을 통한 확실한 멸균 
+                                                보장(Quality Assurance)
+                                            </li>
+                                            <li>
+                                                <i class="circle-num or">04</i>
+                                                물과 산소로 분해되기 때문에 작업자에게 
+                                                안전
+                                            </li>
+                                            <li>
+                                                <i class="circle-num or">05</i>
+                                                검증된 재질 적합성 데이터
+                                            </li>
+                                        </ol>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-lg-4">
+                                        <h5 class="list-title bl">H2O2  증기 멸균의 효율성</h5>
+                                        <ol class="decimal-li">
+                                            <li>
+                                                <i class="circle-num bl">01</i>
+                                                멸균 대상 구역의 온/습도 그대로 이용<br class="visible-lg"> 
+                                                (제습단계 없음)
+                                            </li>
+                                            <li class="bl-txt">
+                                                <i class="circle-num bl">02</i>
+                                                Dual Circuit Tecnology 적용
+                                            </li>
+                                            <li>
+                                                <i class="circle-num bl">03</i>
+                                                특허 등록된 순간증발기(Evaporator)에<br class="visible-lg"> 
+                                                의한 빠른 과산화수소증기 생성
+                                            </li>
+                                            <li>
+                                                <i class="circle-num bl">04</i>
+                                                미세응축(Microcondensation) 형성에 의한<br class="visible-lg">  확실한 표면 멸균 (Surface Sterilization)
+                                            </li>
+                                            <li>
+                                                <i class="circle-num bl">05</i>
+                                                고성능 촉매(Ruthenium)에 의한 환경친화적<br class="visible-lg">  분해(물과 산소 생성)
+                                            </li>
+                                            <li>
+                                                <i class="circle-num bl">06</i>
+                                                Generator 내부에 장착된 각종 센서(온/습도<br class="visible-lg"> 센서, 과산화수소 농도센서 등)에 
+                                                의한 실시간<br class="visible-lg">  모니터링
+                                            </li>
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="container">
+                    <article class="dual-circuit">
+                        <div class="article-header">
+                            <h4 class="content-title">Dual Circuit Tecnology란?</h4>
+                            <hr>
+                        </div>
+                        <div class="article-content">
+                            <p class="figure">
+                                <img src="/assets/images/technology/dual_circuit_xs_img.png" class="img-responsive visible-xs" alt="Dual Circuit Tecnology 이미지">
+                                <img src="/assets/images/technology/dual_circuit_sm_img.png" class="img-responsive visible-sm visible-md" alt="Dual Circuit Tecnology 이미지">
+                                <img src="/assets/images/technology/dual_circuit_lg_img.png" class="img-responsive visible-lg" alt="Dual Circuit Tecnology 이미지">
+                            </p>
+                            <div class="figcaption sr-only">
+                                <h1>
+                                    H<sup>2</sup>O<sup>2</sup> 증기 생성 회로와 분해 회로의 분리로 멸균시간 단축과 H2O2 사용량을 감소
+                                </h1>
+                                <h2>
+                                    HPV Generator의 최첨단 Dual Circuit Technology
+                                </h2>
+                            </div>
+                        </div>
+                    </article>
+                    <div class="table-wrap table-responsive">
+                        <h6>H<sup>2</sup>O<sup>2</sup> 증기와 에어로졸(가습기 타입), 이산화염소 가스와의 차이점</h6>
+                        <table class="table table-bordered">
+                             <colgroup>
+                                 <col style="width: 15%;">
+                                 <col style="width: 28%;">
+                                 <col style="width: 28%;">
+                                 <col style="width: 28%;">
+                             </colgroup>
+                             <thead>
+                                 <tr>
+                                     <th>검증내용</th>
+                                     <th>과산화수소 증기</th>
+                                     <th>에어로졸 (가습기타입) </th>
+                                     <th>이산화염소 가스</th>
+                                 </tr>
+                             </thead>
+                             <tbody>
+                                 <tr>
+                                     <td>멸균제</td>
+                                     <td>과산화수소 30~35%<br>수용액기화, 증기분사</td>
+                                     <td>과산화수소 5~7% 수용액<br>+Ag(은) Fog/미스트 분사</td>
+                                     <td>이산화염소<br>가스 분사</td>
+                                 </tr>
+                                 <tr>
+                                     <td>안정성</td>
+                                     <td>
+                                         OEL<sup>1</sup>(PEL<sup>2</sup>) : 1ppm<br>
+                                         Side effect : Relatively Safe<br>
+                                         IDLH<sup>3</sup> : 75 ppm
+                                     </td>
+                                     <td>No data</td>
+                                     <td>
+                                         OEL<sup>1</sup>(PEL<sup>2</sup>) : 0.1ppm<br>
+                                         Side effect : Very toxic<br>
+                                         IDLH<sup>3</sup> : 10 ppm
+                                     </td>
+                                 </tr>
+                                 <tr>
+                                     <td>입자크기</td>
+                                     <td>0.1 나노</td>
+                                     <td>8 ~ 12 마이크로</td>
+                                     <td>-</td>
+                                 </tr>
+                                 <tr>
+                                     <td>멸균력</td>
+                                     <td>멸균</td>
+                                     <td>소독수준</td>
+                                     <td>살균/소독 수준</td>
+                                 </tr>
+                                 <tr>
+                                     <td>잔류물</td>
+                                     <td>없음</td>
+                                     <td>높음</td>
+                                     <td>높음</td>
+                                 </tr>
+                                 <tr>
+                                     <td>부식성</td>
+                                     <td>낮음</td>
+                                     <td>높음</td>
+                                     <td>높음</td>
+                                 </tr>
+                                 <tr>
+                                     <td>폭발성</td>
+                                     <td>낮음 (물과 산소로 분해)</td>
+                                     <td>-</td>
+                                     <td>(고농도)</td>
+                                 </tr>
+                                 <tr>
+                                     <td>멸균 검증</td>
+                                     <td class="red-txt">가능 ( 6log Reduction)</td>
+                                     <td>불가능</td>
+                                     <td>가능 (3~4log Reduction)</td>
+                                 </tr>
+                             </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+	    </section>
+	</main>
+	<!-- //Sub Content End -->
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ technology/space.php
@@ -0,0 +1,152 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/technology.css" rel="stylesheet">
+</head>
+<body class="sub">
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/top-nav.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/nav-aside.php'); ?>
+	
+	<!-- Sub Content Start -->
+	<main id="content" class="technology">
+        <!-- Page Header -->
+	    <div class="page-header">
+            <div class="page-info">
+                <h2 class="hidden-xs">SERVICE</h2>
+                <p>기술서비스 상품</p>
+                <div class="btn-link-area">
+                    <a href="#" class="btn-link pre-btn" role="button">
+                        <img src="/assets/images/sub/left_link_btn.png" alt="이전 페이지">
+                        <span class="sr-only">이전 페이지</span>
+                    </a>
+                    <a href="#" class="btn-link next-btn" role="button">
+                        <img src="/assets/images/sub/right_link_btn.png" alt="다음 페이지">
+                        <span class="sr-only">이전 페이지</span>
+                    </a>
+                </div>
+	        </div>
+            <div class="lnb dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    과산화수소멸균 서비스
+                    <i class="more-icon">
+                        <span class="sr-only">더보기</span>
+                    </i>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a href="/technology/maintenance.php">유지보수 서비스</a></li>
+                    <li class="active"><a href="/technology/peroxide.php">과산화수소멸균 서비스</a></li>
+                    <li><a href="#">장비검교정 서비스</a></li>
+                    <li><a href="#">우정특성화 서비스</a></li>
+                </ul>
+            </div>
+	    </div>
+	    <!-- //Page Header -->
+	    <section class="space">
+            <div class="section-header">
+                <h3 class="page-title">과산화수소멸균 서비스</h3>
+            </div>
+            <div class="section-content">
+                <div class="container">
+                    <nav class="nav-tabs tab-link-list">
+                        <ul>
+                            <li>
+                                <a href="/technology/peroxide.php">과산화수소증기멸균</a>
+                            </li>
+                            <li class="active">
+                                <a href="/technology/space.php">공간멸균서비스</a>
+                            </li>
+                            <li>
+                                <a href="/technology/process.php">멸균공정개발서비스</a>
+                            </li>
+                            <li>
+                                <a href="/technology/automation.php">멸균자동화 설비 공사</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <article class="room-bio">
+                        <div class="article-header">
+                            <h4 class="content-title">Room Bio-Decontamination Service</h4>
+                            <hr>
+                            <p class="summary">
+                                예산과 효율을 고려한 최선의 선택! 우정비에스씨는<br class="visible-xs">
+                                다년간 축적된 노하우를 바탕으로<br>
+                                숙련된 멸균전문가가 제약회사, 병원, 연구소, BSL3,<br class="visible-xs">
+                                BSL4, 실험동물실, 감염재난시설 등 필요한<br>
+                                공간 및 장비에 대해 과산화수소 멸균 대행 서비스를<br class="visible-xs">
+                                제공합니다.
+                            </p>
+                        </div>
+                        <div class="article-content">
+                            <ul class="row">
+                                <li class="col-xs-6 col-sm-4 col-lg-2">
+                                    <p class="figure">
+                                        <img src="/assets/images/technology/space_list_img01.gif" alt="">
+                                    </p>
+                                    <p class="figcaption">
+                                        설비 투자 없이 최고의<br>
+                                        멸균법인 과산화수소<br>
+                                        증기 멸균 이용 가능
+                                    </p>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-lg-2">
+                                    <p class="figure">
+                                        <img src="/assets/images/technology/space_list_img02.gif" alt="">
+                                    </p>
+                                    <p class="figcaption">
+                                        계획서에서부터<br>
+                                        보고서까지 완벽한<br>
+                                        Protocol 제공
+                                    </p>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-lg-2">
+                                    <p class="figure">
+                                        <img src="/assets/images/technology/space_list_img03.gif" alt="">
+                                    </p>
+                                    <p class="figcaption">
+                                        Cycle Development<br>
+                                        에서 검증까지 인증된<br>
+                                        기술팀이 대행
+                                    </p>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-lg-2">
+                                    <p class="figure">
+                                        <img src="/assets/images/technology/space_list_img04.gif" alt="">
+                                    </p>
+                                    <p class="figcaption">
+                                        Bioquell社의 기술<br>
+                                        자문을 바탕으로<br>
+                                        각종 기술 지원 가능
+                                    </p>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-lg-2">
+                                    <p class="figure">
+                                        <img src="/assets/images/technology/space_list_img05.gif" alt="">
+                                    </p>
+                                    <p class="figcaption">
+                                        사용자의 요구에<br>
+                                        맞도록 고객 맞춤형<br>
+                                        서비스 제공 가능
+                                    </p>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-lg-2">
+                                    <p class="figure">
+                                        <img src="/assets/images/technology/space_list_img06.gif" alt="">
+                                    </p>
+                                    <p class="figcaption">
+                                        긴급 멸균에서 정기적<br>
+                                        멸균 및 기술팀 지원까지<br>
+                                        모든 옵션 고려 가능
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+            </div>
+	    </section>
+	</main>
+	<!-- //Sub Content End -->
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
