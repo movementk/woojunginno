52ece076281393f331e1dd15f55d42ec9100bf04
kimyh5392 <mk.kimyh@gmail.com>
2017-06-02 18:29:45 +0900

    우정콜센터 > 글쓰기 추가 , 기술서비스상품 > 빌트인 작업중 . .

--- center/notice_write.php
+++ center/notice_write.php
@@ -15,11 +15,11 @@
                 <h2 class="hidden-xs">CALL CENTER</h2>
                 <p>우정콜센터</p>
                 <div class="btn-link-area">
-                    <a href="#" class="btn-link pre-btn" role="button">
+                    <a href="/center/satisfy.php" class="btn-link pre-btn" role="button">
                         <img src="/assets/images/sub/left_link_btn.png" alt="이전 페이지">
                         <span class="sr-only">이전 페이지</span>
                     </a>
-                    <a href="/center/satisfy.php" class="btn-link next-btn" role="button">
+                    <a href="/center/as_search.php" class="btn-link next-btn" role="button">
                         <img src="/assets/images/sub/right_link_btn.png" alt="다음 페이지">
                         <span class="sr-only">다음 페이지</span>
                     </a>
@@ -27,29 +27,25 @@
 	        </div>
             <div class="lnb dropdown">
                 <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    1:1문의하기
+                    공지사항
                     <i class="more-icon">
                         <span class="sr-only">더보기</span>
                     </i>
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <li><a href="/center/notice_list.php">공지사항</a></li>
+                    <li class="active"><a href="/center/notice_list.php">공지사항</a></li>
                     <li><a href="/center/as_search.php">A/S현황조회</a></li>
                     <li><a href="#">검교정주기조회</a></li>
-                    <li class="active"><a href="/center/inquiry_list.php">1:1문의하기</a></li>
+                    <li><a href="/center/inquiry_list.php">1:1문의하기</a></li>
                     <li><a href="/center/satisfy.php">고객만족도평가</a></li>
                 </ul>
             </div>
 	    </div>
 	    <!-- //Page Header -->
-	    <section class="inquiry-write write">
+	    <section class="notice-write write">
             <div class="container">
                 <div class="section-header">
-                    <h3 class="page-title">1:1문의하기</h3>
-                    <p class="summary">
-                        궁금하신 사항이 있으시다면 문의해주세요!<br>
-                        빠르고 친절하게 답변드리겠습니다.
-                    </p>
+                    <h3 class="page-title">공지사항</h3>
                 </div>
                 <div class="section-content">
                     <form action="#">
