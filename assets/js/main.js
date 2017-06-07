dea1d301970c5c5afbe309006955e9b2d85c59f7
mk-kimjh <mk.kimjh@gmail.com>
2017-06-07 17:36:38 +0900

    메인페이지 1차 완료

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+(function($) {
+	$('#introduce .intro-slider').bxSlider({
+		mode: 'fade',
+		controls: false,
+		auto: true,
+		autoControls: true,
+		autoControlsCombine: true,
+		autoHover: true,
+		pause: 8000,
+		startText: '<i class="icon-play"></i>',
+		stopText: '<i class="icon-pause"></i>',
+		buildPager: function(slideIndex) {
+			return "<span class='sr-only'>"+slideIndex+"</span>";
+		},
+		onSliderLoad: function(currentIndex) {
+			window.setTimeout(function() {
+				$('#introduce .intro-slider .intro-item').eq(currentIndex).addClass('active');
+			}, 1000);
+		},
+		onSlideAfter: function($slideElement, oldIndex, newIndex) {
+			$('#introduce .intro-slider .intro-item').removeClass('active');
+			$slideElement.addClass('active');
+		}
+	});
+
+	$('#news .news-list ul').bxSlider({
+		mode: "vertical",
+		pager: false,
+		auto: true,
+		autoHover: true,
+		prevText: "<i class='icon-up-micro'></i>",
+		nextText: "<i class='icon-down-micro'></i>",
+	});
+
+	$('#products .product-list ul').bxSlider({
+		pager: false,
+		auto: true,
+		autoHover: true,
+		prevText: "<i class='icon-left-open-big'></i>",
+		nextText: "<i class='icon-right-open-big'></i>",
+	});	
+})(jQuery);
